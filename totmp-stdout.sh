c4a268d74f3308edd92796dc1dff85382ddedd8a
Willian Silva <willian.silva@evobiolab.com>
2024-11-23 11:43:38 +0100

    Update totmp-stdout.sh

--- totmp-stdout.sh
+++ totmp-stdout.sh
@@ -7,42 +7,27 @@
 ##SCRIPT DESCRIPTION:
 
 ##Description:
-##Move target files/directories to the project tmp directory (${PATHTOPROJTMP}).
+##Copy files to project tmp directory.
 
 ##Input: Target files/directories.
-##Output: Move target files/directories to the project tmp directory.
-##Use fromtmp-stdout.sh <TAG> to move target files/directories the specified directory.
+##Output: Copies of files in project tmp directory.
 
 ##Usage: 
-##totmp-stdout.sh <TARGET DIRECTORY>
+##totmp-stdout.sh <FILES>
 
 ############################################################################
 ##ACTIONS:
-
-RUNDATE=$(date +"%Y%m%d%H%M%S")
 
 ##Input.
 INPUT="$@"
 
 ##Process.
-
-#Recovery tag and directory name.
-RECOVERYDIRNAME=$(echo "tmp-${RUNDATE}-$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10)")
-RECOVERYDIR=$(echo "${PATHTOPROJTMP}/${RECOVERYDIRNAME}")
-
-#Create tmp recovery directory.
-mkdir -p ${RECOVERYDIR} 
-
-#Move input files/directories.
-mv ${INPUT} ${RECOVERYDIR} 
-
-echo "----------------------------------------------
-The following files/directories can now be found in ${RECOVERYDIR}:
-$(find ${INPUT} -maxdepth 0)
-
-To recover the files/directories above, use:
-\$ fromtmp-stdout.sh <OUTPUT LOCATION> ${RECOVERYDIRNAME}
-----------------------------------------------"
+IFS=$' '
+find ${INPUT} -maxdepth 0 | while read F ; do
+	cp -r ${F} ${PATHTOPROJTMP}
+	echo "Copy of file ${F} created in directory ${PATHTOPROJTMP}"
+done
+IFS='$ORIGINALIFS'
 
 ############################################################################
 ##END OF BASH SCRIPT...
