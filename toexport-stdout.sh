b6d8d62d9070eb110402ef9b0ae27eb5ac39f6e0
Willian Silva <willian.silva@evobiolab.com>
2024-11-23 10:52:56 +0100

    Update toexport-stdout.sh

--- toexport-stdout.sh
+++ toexport-stdout.sh
@@ -22,10 +22,12 @@
 INPUT="$@"
 
 ##Process.
+IFS=$' '
 find ${INPUT} -maxdepth 0 | while read F ; do
 	cp -r ${F} ${PATHTOPROJEXPORT}
 	echo "Copy of file ${F} created in directory ${PATHTOPROJEXPORT}"
 done
+IFS='$ORIGINALIFS'
 
 ############################################################################
 ##END OF BASH SCRIPT...
