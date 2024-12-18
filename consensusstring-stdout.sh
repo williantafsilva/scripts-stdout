9a0d129e6ad07a1f5a3c0cc6240b4d2f04969763
Willian Silva <willian.silva@evobiolab.com>
2024-12-18 23:04:28 +0100

    Update consensusstring-stdout.sh

--- consensusstring-stdout.sh
+++ consensusstring-stdout.sh
@@ -28,19 +28,10 @@ CONSENSUS=""
 while read C ; do
     C1=$(echo ${C} | cut -d' ' -f1)
     C2=$(echo ${C} | cut -d' ' -f2)
-    echo "${C}"
-    echo ${C} | cut -d' ' -f1
-    echo ${C} | cut -d' ' -f2
-    echo "C1:${C1}"
-    echo "C2:${C2}"
     if [[ "${C1}" == "${C2}" ]] ; then
         CONSENSUS+="${C1}"
-        echo "."
-        echo "CONSENSUS:${CONSENSUS}"
     else
         CONSENSUS+="X"
-        echo "X"
-        echo "CONSENSUS:${CONSENSUS}"
     fi
 done <<< "$(paste -d'\t' <(echo ${STRING1} | grep -o .) <(echo ${STRING2} | grep -o .))"
 echo "CONSENSUS:${CONSENSUS}"
