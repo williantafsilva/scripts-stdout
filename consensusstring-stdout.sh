3ab892451d9b20b12e14ccac39a7fb7f97bfa826
Willian Silva <willian.silva@evobiolab.com>
2024-12-18 20:37:40 +0100

    Create consensusstring-stdout.sh

---
+++ consensusstring-stdout.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+############################################################################
+############################ STDOUT FUNCTION ###############################
+############################################################################
+#Author: Willian T.A.F. Silva (willian.silva@evobiolab.com).
+############################################################################
+##SCRIPT DESCRIPTION:
+
+##Description:
+##Show similarities and differences between two lists.
+
+##Input 1: String 1.
+##Input 2: String 2.
+##Output: Consensus string, with characters that differ between strings substituted by X.
+
+##Usage: 
+##consensusstring-stdout.sh <STRING 1> <STRING 2>
+
+############################################################################
+##ACTIONS:
+
+##Input file.
+STRING1=$1
+STRING2=$2
+
+##Process.
+CONSENSUS=""
+while read C ; do
+    C1=$(echo ${C} | cut -f1)
+    C2=$(echo ${C} | cut -f2)
+    if [[ "${C1}" == "${C2}" ]] ; then
+        CONSENSUS+="${C1}"
+    else
+        CONSENSUS+="X"
+    fi
+done <<< "$(paste -d'\t' <(echo ${STRING1} | grep -o .) <(echo ${STRING2} | grep -o .))"
+echo ${CONSENSUS}
+
+############################################################################
+##END OF BASH SCRIPT...
+############################################################################
